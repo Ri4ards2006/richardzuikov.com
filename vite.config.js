e0b1ee974a0eff0eacb8799f1d8a1015a7506468
Richard Zuikov <astroricards@gmail.com>
2026-05-20 21:56:48 +0200

    refetching once again and making new Frontend Stack

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: './',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'main/index.html'),
-        about: resolve(__dirname, 'main/about.html'),
-        career: resolve(__dirname, 'career/index.html'),
-        lab: resolve(__dirname, 'lab/index.html'),
-        docs: resolve(__dirname, 'docs/index.html'),
-        dev: resolve(__dirname, 'dev/index.html')
-      }
-    }
+  plugins: [react()],
+  server: {
+    port: 5173
   }
 });
