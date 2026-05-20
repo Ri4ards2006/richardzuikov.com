eecfd8be6f95e1ae7183dc809d6fe6fba0103809
Richard Zuikov <astroricards@gmail.com>
2026-05-20 21:02:41 +0200

    reworking and refetching the new scss and the packages for the ultra sigma vite workflow

--- vit.config.js
+++ vit.config.js
@@ -2,26 +2,18 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: './', // Projekt-Wurzel
+  root: './',
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        // Hier definieren wir die Einstiegspunkte für deine Subdomains
         main: resolve(__dirname, 'main/index.html'),
+        about: resolve(__dirname, 'main/about.html'),
         career: resolve(__dirname, 'career/index.html'),
-        dev: resolve(__dirname, 'dev/index.html'),
         lab: resolve(__dirname, 'lab/index.html'),
         docs: resolve(__dirname, 'docs/index.html'),
-      },
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // Hier können wir später globale Variablen (wie dein beiges Theme) hinterlegen
-        api: 'modern-compiler'
-      },
-    },
-  },
+        dev: resolve(__dirname, 'dev/index.html')
+      }
+    }
+  }
 });
